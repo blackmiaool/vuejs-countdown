e51a136b24039c76f1f45a530ea315975b07ec65
blackmiaool <blackmiaool@gmail.com>
2019-01-01 14:44:45 +0800

    reactive

--- src/Countdown.vue
+++ src/Countdown.vue
@@ -2,31 +2,29 @@
     <ul class="vuejs-countdown">
         <li v-if="days > 0">
             <p class="digit">{{ days | twoDigits }}</p>
-            <p class="text">{{ days > 1 ? 'days' : 'day' }}</p>
+            <p class="text">天</p>
         </li>
-        <li>
+        <li v-if="hours>0">
             <p class="digit">{{ hours | twoDigits }}</p>
-            <p class="text">{{ hours > 1 ? 'hours' : 'hour' }}</p>
+            <p class="text">小时</p>
         </li>
-        <li>
+        <li v-if="minutes>0">
             <p class="digit">{{ minutes | twoDigits }}</p>
-            <p class="text">min</p>
+            <p class="text">分</p>
         </li>
         <li>
             <p class="digit">{{ seconds | twoDigits }}</p>
-            <p class="text">Sec</p>
+            <p class="text">秒</p>
         </li>
     </ul>
 </template>
 
 <script>
-let interval = null;
-
 export default {
-    name: 'vuejsCountDown',
+    name: "vuejsCountDown",
     props: {
         deadline: {
-            type: String
+            type: Number
         },
         end: {
             type: String
@@ -37,106 +35,121 @@ export default {
     },
     data() {
         return {
-            now: Math.trunc((new Date()).getTime() / 1000),
-            date: null,
+            interval: null,
+            now: Math.trunc(new Date().getTime() / 1000),
             diff: 0
-        }
+        };
     },
     created() {
         if (!this.deadline && !this.end) {
             throw new Error("Missing props 'deadline' or 'end'");
         }
 
-        let endTime = this.deadline ? this.deadline : this.end;
-        this.date = Math.trunc(Date.parse(endTime.replace(/-/g, "/")) / 1000);
-
         if (!this.date) {
-            throw new Error("Invalid props value, correct the 'deadline' or 'end'");
+            throw new Error(
+                "Invalid props value, correct the 'deadline' or 'end'"
+            );
         }
-
-        interval = setInterval(() => {
-            this.now = Math.trunc((new Date()).getTime() / 1000);
-        }, 1000);
+        this.tick();
+        this.interval = setInterval(this.tick.bind(this), 1000);
+    },
+    watch: {
+        date: {
+            immediate: true,
+            handler() {
+                if (!this.interval) {
+                    this.interval = setInterval(this.tick.bind(this), 1000);
+                }
+            }
+        }
+    },
+    methods: {
+        tick() {
+            this.now = Math.trunc(new Date().getTime() / 1000);
+            this.diff = this.date - this.now;
+            if (this.diff <= 0 || this.stop) {
+                this.diff = 0;
+                // Remove interval
+                clearInterval(this.interval);
+                this.interval = null;
+            }
+        }
     },
     computed: {
+        date() {
+            let endTime = this.deadline ? this.deadline : this.end;
+            return endTime / 1000;
+        },
         seconds() {
-            return Math.trunc(this.diff) % 60
+            return Math.trunc(this.diff) % 60;
         },
 
         minutes() {
-            return Math.trunc(this.diff / 60) % 60
+            return Math.trunc(this.diff / 60) % 60;
         },
 
         hours() {
-            return Math.trunc(this.diff / 60 / 60) % 24
+            return Math.trunc(this.diff / 60 / 60) % 24;
         },
 
         days() {
-            return Math.trunc(this.diff / 60 / 60 / 24)
-        }
-    },
-    watch: {
-        now(value) {
-            this.diff = this.date - this.now;
-            if(this.diff <= 0 || this.stop){
-                this.diff = 0;
-                // Remove interval
-                clearInterval(interval);
-            }
+            return Math.trunc(this.diff / 60 / 60 / 24);
         }
     },
     filters: {
         twoDigits(value) {
-            if ( value.toString().length <= 1 ) {
-                return '0'+value.toString()
+            if (value.toString().length <= 1) {
+                return "0" + value.toString();
             }
-            return value.toString()
+            return value.toString();
         }
     },
     destroyed() {
-        clearInterval(interval);
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
     }
-}
+};
 </script>
-<style>
+<style scoped>
 .vuejs-countdown {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 .vuejs-countdown li {
-  display: inline-block;
-  margin: 0 8px;
-  text-align: center;
-  position: relative;
+    display: inline-block;
+    margin: 0 8px;
+    text-align: center;
+    position: relative;
 }
 .vuejs-countdown li p {
     margin: 0;
 }
 .vuejs-countdown li:after {
-  content: ":";
-  position: absolute;
-  top: 0;
-  right: -13px;
-  font-size: 32px;
+    content: ":";
+    position: absolute;
+    top: 0;
+    right: -13px;
+    font-size: 32px;
 }
 .vuejs-countdown li:first-of-type {
-  margin-left: 0;
+    margin-left: 0;
 }
 .vuejs-countdown li:last-of-type {
-  margin-right: 0;
+    margin-right: 0;
 }
 .vuejs-countdown li:last-of-type:after {
-  content: "";
+    content: "";
 }
 .vuejs-countdown .digit {
-  font-size: 32px;
-  font-weight: 600;
-  line-height: 1.4;
-  margin-bottom: 0;
+    font-size: 32px;
+    font-weight: 600;
+    line-height: 1.4;
+    margin-bottom: 0;
 }
 .vuejs-countdown .text {
-  text-transform: uppercase;
-  margin-bottom: 0;
-  font-size: 10px;
+    text-transform: uppercase;
+    margin-bottom: 0;
+    font-size: 10px;
 }
 </style>
